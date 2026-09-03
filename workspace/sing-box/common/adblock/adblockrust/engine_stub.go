9f6e4c854d846c36c1f15759428169f612fc9525
eleutherifer <eleutherifer@gmail.com>
2026-09-03 13:07:04 +0300

    Delete workspace directory

--- workspace/sing-box/common/adblock/adblockrust/engine_stub.go
+++
@@ -1,9 +0,0 @@
-//go:build !with_adblock
-
-package adblockrust
-
-import E "github.com/sagernet/sing/common/exceptions"
-
-func newEngine(ruleSets []RuleSet, adblockResources string) (Engine, error) {
-	return nil, E.New("adblock is not included in this build, rebuild with -tags with_adblock")
-}
--- workspace/sing-box/dns/transport_adapter_outbound.go
+++
@@ -1,5 +0,0 @@
-package dns
-
-func (a *TransportAdapter) DNSOutbound() (string, bool) {
-	return a.outbound, a.hasOutbound
-}
